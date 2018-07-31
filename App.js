07beda85fc260065ab70b7241c869a207714e245
Tom <TomSimmons@Toms-MacBook-Pro.local>
2018-07-31 13:56:29 +0100

    add mapboxgl and set up journey overview modal

--- App.js
+++ App.js
@@ -2,46 +2,57 @@ import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
 import LoginRegisterScreen from "./components/LoginRegisterScreen";
 import Home from "./components/Home";
-import * as api from './api'
+import * as api from "./api";
 
 export default class App extends Component<Props> {
-  state = { currentUser: { username: "" }, register: false };
+  state = { currentUser: { username: "yom" }, register: false };
 
   // signup
   signup = async (email, username) => {
-    const avatar = 'https://images.pexels.com/photos/301448/pexels-photo-301448.jpeg?auto=compress&cs=tinysrgb&h=350'
-    const newUser = {email, username, avatar}
-    await api.createUser(newUser)
-      .then((user) => {
-        this.setState({ currentUser: {
-          username:user.username
-        } });
+    const avatar =
+      "https://images.pexels.com/photos/301448/pexels-photo-301448.jpeg?auto=compress&cs=tinysrgb&h=350";
+    const newUser = { email, username, avatar };
+    await api
+      .createUser(newUser)
+      .then(user => {
+        this.setState({
+          currentUser: {
+            username: user.username
+          }
+        });
       })
-      .catch(console.log)
+      .catch(console.log);
   };
 
   //login
-  login = async (email) => {
-    await api.fetchUserByEmail(email)
-    .then((user) => {
-      this.setState({currentUser: {
-        username:user.username
-      }})
-    })
-    .catch(console.log)
-  }
+  login = async email => {
+    await api
+      .fetchUserByEmail(email)
+      .then(user => {
+        this.setState({
+          currentUser: {
+            username: user.username
+          }
+        });
+      })
+      .catch(console.log);
+  };
 
   logout = () => {
     this.setState({
-      currentUser: { username: '' }
-    })
-  }
+      currentUser: { username: "" }
+    });
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        {this.state.currentUser.username === "" && <LoginRegisterScreen login={this.login} signup={this.signup}/>}
-        {this.state.currentUser.username !== "" && <Home logout={this.logout}/>}
+        {this.state.currentUser.username === "" && (
+          <LoginRegisterScreen login={this.login} signup={this.signup} />
+        )}
+        {this.state.currentUser.username !== "" && (
+          <Home logout={this.logout} />
+        )}
       </View>
     );
   }
@@ -56,5 +67,3 @@ const styles = StyleSheet.create({
     backgroundColor: "rgb(0,220,90)"
   }
 });
-
-
