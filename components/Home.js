39edb3135060969163aeaf48c818a4543fd7d1c5
Tom <TomSimmons@Toms-MacBook-Pro.local>
2018-07-31 16:26:29 +0100

    change lat/long mishap and menu burger update

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
-import Permissions from 'react-native-permissions'
+import Permissions from "react-native-permissions";
 import TransportCard from "./TransportCard";
 import CounterModal from "./CounterModal";
-import MenuCard from "./MenuCard"
-import * as api from "../api"
+import MenuCard from "./MenuCard";
+import * as api from "../api";
 import UserStats from "./UserStats";
 import JourneyOverviewModal from "./JourneyOverviewModal";
 
@@ -13,109 +13,119 @@ class Home extends Component {
     modalVisible: false,
     mode: "",
     recording: false,
-    menuVisible: false, 
-    locationPermission: '',
+    menuVisible: false,
+    locationPermission: "",
     coords: [],
     currentJourney: null,
     loggedJourney: null,
     journey: { startTime: "", endTime: "", coords: [] }, //Is this used?
     journeyModalVisible: false
-
   };
 
   //geolocation
-    getLatLong = () => new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition((position) => {
+  getLatLong = () =>
+    new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        position => {
           resolve(position.coords);
-      }, (error) => {
+        },
+        error => {
           reject(error);
-      }, { enableHighAccuracy: false, timeout: 30000, maximumAge: 10000 });
+        },
+        { enableHighAccuracy: false, timeout: 30000, maximumAge: 10000 }
+      );
     });
 
-    startCoords = () => {
-      this.getLatLong()
-        .then((newCoords) => {
-          const {latitude, longitude} = newCoords;
-          const currentCoords = {lat: latitude, long: longitude, time: Date.now()}
-          this.setState({
-            coords: [...this.state.coords, currentCoords]
-          })
-        })
-        .catch(console.log)      
-    }
-
-    endCoords = () => {
-      this.getLatLong()
-        .then((newCoords) => {
-          console.log(newCoords)
-          const {latitude, longitude} = newCoords;
-          const currentCoords = {lat: longitude, long: longitude, time: Date.now()}
-          this.setState({
-            coords: [...this.state.coords, currentCoords]
-          })
-          const newJourney = {
-            mode: this.state.mode,
-            route: this.state.coords,
-            belongs_to: this.props.user._id
-          }
-          console.log(newJourney, 'from first .then')
-          return newJourney
-        })
-        .then((newJourney) => {
-          console.log(newJourney, 'from second .then')
-          return api.createJourney(newJourney)    
-        })
-        .then((res) => {
-          console.log(res)
-          const {newJourney} = res.data
-          this.setState({
-            currentJourney: newJourney
-          })
-          console.log(newJourney)
-        })
-        .catch(console.log)  
-      
-    }   
-
-    //permissions for geolocation - component did mount alerts for permission
-    //when data is first installed, then stores in device's location
-    _requestPermission = () => {
-      Permissions.request('location').then(response => {
-        // Returns once the user has chosen to 'allow' or to 'not allow' access
-        // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
-        this.setState({ locationoPermission: response })
+  startCoords = () => {
+    this.getLatLong()
+      .then(newCoords => {
+        const { latitude, longitude } = newCoords;
+        const currentCoords = {
+          lat: latitude,
+          long: longitude,
+          time: Date.now()
+        };
+        this.setState({
+          coords: [...this.state.coords, currentCoords]
+        });
       })
-    }
+      .catch(console.log);
+  };
 
-    _alertForLocationPermission() {
-      Alert.alert(
-        'Can we access your location?',
-        'We need access so we can calculate your carbon emissions when traveling',
-        [
-          {
-            text: 'No way',
-            onPress: () => console.log('Permission denied'),
-            style: 'cancel',
-          },
-          this.state.locationPermission == 'undetermined'
-            ? { text: 'OK', onPress: this._requestPermission }
-            : { text: 'Open Settings', onPress: Permissions.openSettings },
-        ],
-      )
-    }
-
-    componentDidMount() {
-      Permissions.request('location').then(response => {
-        // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
-        this.setState({ locationPermission: response })
+  endCoords = () => {
+    this.getLatLong()
+      .then(newCoords => {
+        console.log(newCoords);
+        const { latitude, longitude } = newCoords;
+        const currentCoords = {
+          lat: latitude,
+          long: longitude,
+          time: Date.now()
+        };
+        this.setState({
+          coords: [...this.state.coords, currentCoords]
+        });
+        const newJourney = {
+          mode: this.state.mode,
+          route: this.state.coords,
+          belongs_to: this.props.user._id
+        };
+        console.log(newJourney, "from first .then");
+        return newJourney;
       })
-      Permissions.check('location').then(response => {
-        // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
-        this.setState({ locationPermission: response })
+      .then(newJourney => {
+        console.log(newJourney, "from second .then");
+        return api.createJourney(newJourney);
       })
-    }
+      .then(res => {
+        console.log(res);
+        const { newJourney } = res.data;
+        this.setState({
+          currentJourney: newJourney
+        });
+        console.log(newJourney);
+      })
+      .catch(console.log);
+  };
 
-  
+  //permissions for geolocation - component did mount alerts for permission
+  //when data is first installed, then stores in device's location
+  _requestPermission = () => {
+    Permissions.request("location").then(response => {
+      // Returns once the user has chosen to 'allow' or to 'not allow' access
+      // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+      this.setState({ locationoPermission: response });
+    });
+  };
+
+  _alertForLocationPermission() {
+    Alert.alert(
+      "Can we access your location?",
+      "We need access so we can calculate your carbon emissions when traveling",
+      [
+        {
+          text: "No way",
+          onPress: () => console.log("Permission denied"),
+          style: "cancel"
+        },
+        this.state.locationPermission == "undetermined"
+          ? { text: "OK", onPress: this._requestPermission }
+          : { text: "Open Settings", onPress: Permissions.openSettings }
+      ]
+    );
+  }
+
+  componentDidMount() {
+    Permissions.request("location").then(response => {
+      // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+      this.setState({ locationPermission: response });
+    });
+    Permissions.check("location").then(response => {
+      // Response is one of: 'authorized', 'denied', 'restricted', or 'undetermined'
+      this.setState({ locationPermission: response });
+    });
+  }
+
   //menu for logout button
   toggleMenu = () => {
     this.setState({
@@ -131,7 +141,7 @@ class Home extends Component {
   setModalVisible = (bool, transport) => {
     bool
       ? this.setState({ modalVisible: bool, mode: transport })
-      : this.setState({ modalVisible: bool});
+      : this.setState({ modalVisible: bool });
   };
 
   setOverviewVisible = () => {
@@ -161,14 +171,14 @@ class Home extends Component {
               this.setModalVisible(bool, transport)
             }
           />
-          <UserStats user={this.props.user}/>
+          <UserStats user={this.props.user} />
         </View>
 
         <JourneyOverviewModal
           journeyModalVisible={this.state.journeyModalVisible}
           setOverviewVisible={this.setOverviewVisible}
         />
-            
+
         <CounterModal
           recording={this.state.recording}
           toggleRecording={this.toggleRecording}
@@ -177,9 +187,8 @@ class Home extends Component {
           setModalVisible={(bool, transport) =>
             this.setModalVisible(bool, transport)
           }
-          startCoords= {this.startCoords}
-          endCoords= {this.endCoords}
-
+          startCoords={this.startCoords}
+          endCoords={this.endCoords}
         />
       </View>
     );
